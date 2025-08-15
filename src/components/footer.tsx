514f506a6ebeec18bfaad9a675c00f7676574402
Abhishek Jain <jainabhishek356@gmail.com>
2025-08-16 00:29:49 +0530

    minor change in footer make to mak

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full text-center py-4 text-sm text-gray-500 border-t mt-8">
-      © {new Date().getFullYear()} MakeInSec. All rights reserved.
+      © {new Date().getFullYear()} MakInSec. All rights reserved.
     </footer>
   )
 }
